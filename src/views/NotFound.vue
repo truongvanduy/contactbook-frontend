027213da27812548516da0904165528a072144a0
Duy <truongvanduy09072002@gmail.com>
2024-04-03 13:57:27 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="page">
+    <p>
+      Oops, không thể tìm thấy trang. Trở về
+      <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
